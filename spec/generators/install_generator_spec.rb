ecec6cb7a10750f131bb894bec7a60b0f78f0b42
Nick Kugaevsky <nick@kugaevsky.ru>
2012-09-25 02:30:03 +0400

    Test for application.css injecting

--- spec/generators/install_generator_spec.rb
+++ spec/generators/install_generator_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe Mousetrap::Generators::InstallGenerator do
-  # subject { Mousetrap::Generators::InstallGenerator }
-
   context "with mousetrap:install" do
     it "should generate keybindings.js.coffee file" do
       subject.should generate("app/assets/javascripts/keybindings.js.coffee") { |content|
@@ -13,6 +11,12 @@ describe Mousetrap::Generators::InstallGenerator do
     it "should inject require mousetrap into application.js" do
       subject.should generate("app/assets/javascripts/application.js") { |content|
         content.should =~ /\/\/= require mousetrap\n/
+      }
+    end
+
+    it "should inject require mousetrap into application.css" do
+      subject.should generate("app/assets/stylesheets/application.css") { |content|
+        content.should =~ /\s\*= require mousetrap\n/
       }
     end
   end
